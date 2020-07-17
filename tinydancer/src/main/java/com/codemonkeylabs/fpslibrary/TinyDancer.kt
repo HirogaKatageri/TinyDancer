eee04466e4b1d975ed1b3d4176f8809aadf1e2b9
Gian Quintana <gianpatrick27@gmail.com>
2020-07-17 11:37:43 +0800

    0.1.3

--- tinydancer/src/main/java/com/codemonkeylabs/fpslibrary/TinyDancer.kt
+++ tinydancer/src/main/java/com/codemonkeylabs/fpslibrary/TinyDancer.kt
@@ -1,6 +1,11 @@
 package com.codemonkeylabs.fpslibrary
 
 import android.app.Application
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
 import android.view.Choreographer
 import com.codemonkeylabs.fpslibrary.ui.TinyCoach
 
@@ -10,8 +15,29 @@ class TinyDancer {
     private lateinit var tinyCoach: TinyCoach
     private lateinit var fpsFrameCallback: FPSFrameCallback
 
-    fun show() {
+    fun show(context: Context) {
+        if (overlayPermRequest(context)) {
+            //once permission is granted then you must call show() again
+            return
+        }
+
         tinyCoach.show()
+    }
+
+    private fun overlayPermRequest(context: Context): Boolean {
+        var permNeeded = false
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(context)) {
+                val intent = Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + context.packageName)
+                )
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                context.startActivity(intent)
+                permNeeded = true
+            }
+        }
+        return permNeeded
     }
 
     fun hide() {
